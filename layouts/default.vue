fb78fca86875ae7d2246159804d3ceddd705b6fa
tapiwa-1 <tmotsi10@gmail.com>
2023-11-30 10:51:40 +0200

    structing

---
+++ layouts/default.vue
@@ -0,0 +1,39 @@
+<template>
+    <nav class="bg-white border-gray-200 dark:bg-gray-900">
+        <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+            <LogoNav />
+            <div class="flex items-center">
+                <a href="tel:5541251234" class="mr-6 text-sm  text-gray-500 dark:text-white hover:underline">(555)
+                    412-1234</a>
+                <ULink to="/auth/login" active-class="text-primary"
+                    inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200">
+                    Login
+                </ULink>
+            </div>
+        </div>
+    </nav>
+    <nav class="bg-gray-50 dark:bg-gray-700">
+        <div class="max-w-screen-xl px-4 py-3 mx-auto">
+            <div class="flex items-center">
+                <ul class="flex flex-row font-medium mt-0 mr-6 space-x-8 text-sm">
+                    <li>
+                        <a href="#" class="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-gray-900 dark:text-white hover:underline">Company</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-gray-900 dark:text-white hover:underline">Team</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-gray-900 dark:text-white hover:underline">Features</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+<script setup>
+// const { $userStore, $generalStore } = useNuxtApp()
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <NuxtLayout />
+    <UPageHero title="Official Support"
+        description="Through NuxtLabs experts and agency networks, we have a solution for every need." />
+    <UMain>
+        <UContainer>
+            x
+        </UContainer>
+    </UMain>
+    <Footer />
+</template>
