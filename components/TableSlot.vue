e236bc0897c05d80790ef53a87a37100fe08d8c8
tapiwa-1 <tmotsi10@gmail.com>
2023-12-09 08:33:24 +0200

    adding middleware

--- components/TableSlot.vue
+++ components/TableSlot.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="w-full max-w-full lg:max-w-lg p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+        class="w-full max-w-full  p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
         <div class="flex items-center justify-between mb-4">
             <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">{{ tableMeta.heading }}</h5>
             <NuxtLink :to="tableMeta.url" class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
--- pages/admin/roles.vue
+++ pages/admin/roles.vue
@@ -13,34 +13,25 @@
             </div>
             <div
                 class="w-full max-w-full  p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-                <div class="flex items-center justify-between mb-4">
-                    <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Roles</h5>
-                    <a href="#" class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
-                        Add new role
-                    </a>
-                </div>
-                <div class="flow-root">
-                    <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
-                        <li v-for="role in roles" :key="role.title" class="py-3 sm:py-4">
-                            <div class="flex items-center">
-                                <div class="flex-1 min-w-0 ms-4">
-                                    <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                        {{ role.title }}
-                                    </p>
-                                    <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                                        {{ role.description }}
-                                    </p>
-                                </div>
-                                <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                    edit
-                                </div>
+                <TableSlot :table-meta="tableMeta">
+                    <li v-for="role in roles" :key="role.title" class="py-3 sm:py-4">
+                        <div class="flex items-center">
+                            <div class="flex-1 min-w-0 ms-4">
+                                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                    {{ role.title }}
+                                </p>
+                                <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                                    {{ role.description }}
+                                </p>
                             </div>
-                        </li>
+                            <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                                edit
+                            </div>
+                        </div>
+                    </li>
+                </TableSlot>
 
-                    </ul>
-                </div>
             </div>
-
         </div>
     </NuxtLayout>
 </template>
@@ -85,4 +76,10 @@ const roles = ref([
     }
 
 ])
+const tableMeta = ref({
+    heading: "Users",
+    url: null,
+    urlName: "Add Role",
+    addRole: true,
+})
 </script>
